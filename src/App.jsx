975232f38b55982b3d43adbc8010b7bea2c8e83a
dkopylov76 <d.kopylov.76@gmail.com>
2025-08-23 16:03:09 +0300

    comp_09

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css';
-import PopUser from './components/PopUser';
+import "./App.css";
+import PopUser from "./components/PopUser";
 
 function App() {
 
