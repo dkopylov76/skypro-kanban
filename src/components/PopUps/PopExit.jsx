f592d92b41692c87836d66b7caff0355fbd927e4
dkopylov76 <d.kopylov.76@gmail.com>
2025-10-13 22:24:49 +0300

    route_03

--- src/components/PopUps/PopExit.jsx
+++ src/components/PopUps/PopExit.jsx
@@ -1,21 +1,39 @@
+import {
+  PopExitMain,
+  PopExitContainer,
+  PopExitBlock,
+  PopExitTtl,
+  PopExitForm,
+} from './PopExit.styled';
+
 const PopExit = () => {
     return (
         <>
-            <div className="pop-exit" id="popExit">
-				<div className="pop-exit__container">
-					<div className="pop-exit__block">
-						<div className="pop-exit__ttl">
+            <PopExitMain>
+			{/* <div className="pop-exit" id="popExit"> */}
+				<PopExitContainer>
+				{/* <div className="pop-exit__container"> */}
+					<PopExitBlock>
+					{/* <div className="pop-exit__block"> */}
+						<PopExitTtl>
+						{/* <div className="pop-exit__ttl"> */}
 							<h2>Выйти из аккаунта?</h2>
-						</div>
-						<form className="pop-exit__form" id="formExit" action="#">
-							<div className="pop-exit__form-group">
+						</PopExitTtl>
+						{/* </div> */}
+						<PopExitForm>
+						{/* <form className="pop-exit__form" id="formExit" action="#"> */}
+							{/* <div className="pop-exit__form-group"> */}
 								<button className="pop-exit__exit-yes _hover01" id="exitYes"><a href="modal/signin.html">Да, выйти</a> </button>
 								<button className="pop-exit__exit-no _hover03" id="exitNo"><a href="main.html">Нет, остаться</a> </button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
+							{/* </div> */}
+						</PopExitForm>
+						{/* </form> */}
+					</PopExitBlock>
+					{/* </div> */}
+				</PopExitContainer>
+				{/* </div> */}
+			</PopExitMain>
+			{/* </div> */}
         </>
     );
 }
