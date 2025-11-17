eb59b75d87cae13e929a3aeaa68df6f3c5ecb491
dkopylov76 <d.kopylov.76@gmail.com>
2025-11-17 21:31:50 +0300

    context_01

---
+++ src/components/Context/LoginProvider.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { LoginContext } from "./LoginContext";
+import { checkLs } from "../utils/checkLs";
+
+const LoginProvider = ({children}) => {
+    const [user, setUser] = useState(checkLs());
+
+    const updateUserInfo = (userData) => {
+        setUser(userData);
+        if (userData) {
+            localStorage.setItem("userInfo", JSON.stringify(userData));
+        } else {
+            localStorage.removeItem("userInfo");
+        }
+    };
+
+    return (
+        <LoginContext.Provider value={{ user, updateUserInfo }}>
+            {children}
+        </LoginContext.Provider>
+    );
+};
+
+export default LoginProvider;
